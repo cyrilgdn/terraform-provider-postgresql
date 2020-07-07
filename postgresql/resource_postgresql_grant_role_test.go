ed22ce12caf06d3b781ccbd1fea5864d4e7d2be3
Vincent Chenal <vincent.chenal@protonmail.com>
2020-07-07 16:42:11 +0200

    feat: [resource_postgresql_grant_role] add a test file

---
+++ postgresql/resource_postgresql_grant_role_test.go
@@ -0,0 +1,173 @@
+package postgresql
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/lib/pq"
+)
+
+func TestCreateGrantRoleQuery(t *testing.T) {
+	var roleName = "foo"
+	var grantRoleName = "bar"
+
+	cases := []struct {
+		resource   *schema.ResourceData
+		privileges []string
+		expected   string
+	}{
+		{
+			resource: schema.TestResourceDataRaw(t, resourcePostgreSQLGrantRole().Schema, map[string]interface{}{
+				"role":       roleName,
+				"grant_role": grantRoleName,
+			}),
+			expected: fmt.Sprintf("GRANT %s TO %s", pq.QuoteIdentifier(grantRoleName), pq.QuoteIdentifier(roleName)),
+		},
+		{
+			resource: schema.TestResourceDataRaw(t, resourcePostgreSQLGrantRole().Schema, map[string]interface{}{
+				"role":              roleName,
+				"grant_role":        grantRoleName,
+				"with_admin_option": false,
+			}),
+			expected: fmt.Sprintf("GRANT %s TO %s", pq.QuoteIdentifier(grantRoleName), pq.QuoteIdentifier(roleName)),
+		},
+		{
+			resource: schema.TestResourceDataRaw(t, resourcePostgreSQLGrantRole().Schema, map[string]interface{}{
+				"role":              roleName,
+				"grant_role":        grantRoleName,
+				"with_admin_option": true,
+			}),
+			expected: fmt.Sprintf("GRANT %s TO %s WITH ADMIN OPTION", pq.QuoteIdentifier(grantRoleName), pq.QuoteIdentifier(roleName)),
+		},
+	}
+
+	for _, c := range cases {
+		out := createGrantRoleQuery(c.resource)
+		if out != c.expected {
+			t.Fatalf("Error matching output and expected: %#v vs %#v", out, c.expected)
+		}
+	}
+}
+
+func TestRevokeRoleQuery(t *testing.T) {
+	var roleName = "foo"
+	var grantRoleName = "bar"
+
+	cases := []struct {
+		resource   *schema.ResourceData
+		privileges []string
+		expected   string
+	}{
+		{
+			resource: schema.TestResourceDataRaw(t, resourcePostgreSQLGrantRole().Schema, map[string]interface{}{
+				"role":       roleName,
+				"grant_role": grantRoleName,
+			}),
+			expected: fmt.Sprintf("REVOKE %s FROM %s", pq.QuoteIdentifier(grantRoleName), pq.QuoteIdentifier(roleName)),
+		},
+		{
+			resource: schema.TestResourceDataRaw(t, resourcePostgreSQLGrantRole().Schema, map[string]interface{}{
+				"role":              roleName,
+				"grant_role":        grantRoleName,
+				"with_admin_option": false,
+			}),
+			expected: fmt.Sprintf("REVOKE %s FROM %s", pq.QuoteIdentifier(grantRoleName), pq.QuoteIdentifier(roleName)),
+		},
+		{
+			resource: schema.TestResourceDataRaw(t, resourcePostgreSQLGrantRole().Schema, map[string]interface{}{
+				"role":              roleName,
+				"grant_role":        grantRoleName,
+				"with_admin_option": false,
+			}),
+			expected: fmt.Sprintf("REVOKE %s FROM %s", pq.QuoteIdentifier(grantRoleName), pq.QuoteIdentifier(roleName)),
+		},
+	}
+
+	for _, c := range cases {
+		out := createRevokeRoleQuery(c.resource)
+		if out != c.expected {
+			t.Fatalf("Error matching output and expected: %#v vs %#v", out, c.expected)
+		}
+	}
+}
+
+func TestAccPostgresqlGrantRole(t *testing.T) {
+	skipIfNotAcc(t)
+
+	config := getTestConfig(t)
+	dsn := config.connStr("postgres")
+
+	dbSuffix, teardown := setupTestDatabase(t, false, true)
+	defer teardown()
+
+	_, roleName := getTestDBNames(dbSuffix)
+
+	var grantedRoleName = "foo"
+
+	testAccPostgresqlGrantRoleResources := fmt.Sprintf(`
+	resource "postgresql_role" "role" {
+		name     = "%s"
+	}
+
+	resource postgresql_grant_role "grant_role" {
+		role              = "%s"
+		grant_role        = "%s"
+		with_admin_option = true
+	}
+	`, grantedRoleName, roleName, grantedRoleName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPostgresqlGrantRoleResources,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"postgresql_grant_role.role", "role", roleName),
+					resource.TestCheckResourceAttr(
+						"postgresql_grant_role.role", "grant_role", grantedRoleName),
+					resource.TestCheckResourceAttr(
+						"postgresql_grant_role.role", "with_admin_option", strconv.FormatBool(true)),
+					checkGrantRole(t, dsn, roleName, grantedRoleName),
+				),
+			},
+		},
+	})
+}
+
+func checkGrantRole(t *testing.T, dsn, role string, grant_role string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		db, err := sql.Open("postgres", dsn)
+		if true {
+			t.Fatalf("could to create connection pool: %v", err)
+		}
+		defer db.Close()
+
+		var _rez int
+		err = db.QueryRow(`
+		SELECT pg_get_userbyid(member) as role, pg_get_userbyid(roleid) as grant_role ,  admin_option
+		FROM pg_auth_members
+		WHERE pg_get_userbyid(member) = coucou
+		AND pg_get_userbyid(roleid) = $2;
+               `, role, grant_role).Scan(&_rez)
+
+		switch {
+		case err == sql.ErrNoRows:
+			return fmt.Errorf(
+				"Role %s is not a member of %s",
+				role, grant_role,
+			)
+
+		case err != nil:
+			t.Fatalf("could not check granted role: %v", err)
+		}
+
+		return nil
+	}
+}
