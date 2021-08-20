3fb10e75d27bb3f34d1877a68635c1eb9991ea25
Loïc Blot <nerzhul@users.noreply.github.com>
2021-08-20 23:35:49 +0200

    feat: add postgresql_physical_replication_slot (#107)
    * feat: add postgresql_physical_replication_slot
    
    In our usecase at Veepee we use patroni to bootstrap PostgreSQL clusters and terraform to bootstrap users & databases.
    For our Multi Datacenters setup we need fine grain replication slots in order to permit to promote a standby-leader
    and move replication slots at the same time, to prevent empty slots.
    
    The current postgresql_replication_slot is unable to handle the physical and as it's clearly not exactly the same way to handle both
    we created  a custom resource for physical only
    
    * Add documentation

---
+++ postgresql/resource_postgresql_physical_replication_slot.go
@@ -0,0 +1,69 @@
+package postgresql
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourcePostgreSQLPhysicalReplicationSlot() *schema.Resource {
+	return &schema.Resource{
+		Create: PGResourceFunc(resourcePostgreSQLPhysicalReplicationSlotCreate),
+		Read:   PGResourceFunc(resourcePostgreSQLPhysicalReplicationSlotRead),
+		Delete: PGResourceFunc(resourcePostgreSQLPhysicalReplicationSlotDelete),
+		Exists: PGResourceExistsFunc(resourcePostgreSQLPhysicalReplicationSlotExists),
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourcePostgreSQLPhysicalReplicationSlotCreate(db *DBConnection, d *schema.ResourceData) error {
+	name := d.Get("name").(string)
+	sql := "SELECT FROM pg_create_physical_replication_slot($1)"
+	if _, err := db.Exec(sql, name); err != nil {
+		return fmt.Errorf("could not create physical ReplicationSlot %s: %w", name, err)
+	}
+	d.SetId(name)
+
+	return nil
+}
+
+func resourcePostgreSQLPhysicalReplicationSlotExists(db *DBConnection, d *schema.ResourceData) (bool, error) {
+	query := "SELECT 1 FROM pg_catalog.pg_replication_slots WHERE slot_name = $1 and slot_type = 'physical'"
+	var unused int
+	err := db.QueryRow(query, d.Id()).Scan(&unused)
+	switch {
+	case err == sql.ErrNoRows:
+		return false, nil
+	case err != nil:
+		return false, err
+	}
+
+	return true, nil
+}
+
+func resourcePostgreSQLPhysicalReplicationSlotRead(db *DBConnection, d *schema.ResourceData) error {
+	d.Set("name", d.Id())
+	return nil
+}
+
+func resourcePostgreSQLPhysicalReplicationSlotDelete(db *DBConnection, d *schema.ResourceData) error {
+
+	replicationSlotName := d.Get("name").(string)
+
+	if _, err := db.Exec("SELECT pg_drop_replication_slot($1)", replicationSlotName); err != nil {
+		return err
+	}
+
+	d.SetId("")
+	return nil
+}
