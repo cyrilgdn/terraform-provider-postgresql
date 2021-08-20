a06646f18ab27ddba0ecdd93190b92ccb6b59098
Cyril Gaudin <cyril.gaudin@gmail.com>
2021-08-20 23:59:18 +0200

    Disable statement_timeout for connections that need lock.
    Otherwise, blocking timeout could fail.
    Fix #102 #96

--- postgresql/helpers.go
+++ postgresql/helpers.go
@@ -455,6 +455,8 @@ func getRoleOID(db QueryAble, role string) (int, error) {
 
 // Lock a role and all his members to avoid concurrent updates on some resources
 func pgLockRole(txn *sql.Tx, role string) error {
+	// Disable statement timeout for this connection otherwise the lock could fail
+	txn.Exec("SET statement_timeout = 0")
 	if _, err := txn.Exec("SELECT pg_advisory_xact_lock(oid::bigint) FROM pg_roles WHERE rolname = $1", role); err != nil {
 		return fmt.Errorf("could not get advisory lock for role %s: %w", role, err)
 	}
