3699aa28f916824fe296260bcd14fca2a9d93d48
Warnar Boekkooi <warnar.boekkooi@lengoo.de>
2021-12-29 06:30:43 +0100

    Use docker healthcheck
    Use a docker healthcheck with pg-isready instead of a custom bash script. It's just one thing less to maintain.
    
    Also see:
    - https://www.postgresql.org/docs/9.4/app-pg-isready.html
    - https://docs.docker.com/engine/reference/builder/#healthcheck

--- tests/testacc_setup.sh
+++ tests/testacc_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 source "$(pwd)"/tests/switch_superuser.sh
-docker-compose -f "$(pwd)"/tests/docker-compose.yml up -d
-"$(pwd)"/tests/wait-postgres-docker.sh "$(pwd)"/tests/docker-compose.yml
+docker-compose -f "$(pwd)"/tests/docker-compose.yml up -d --wait
