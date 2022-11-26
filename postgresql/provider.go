b8e48709001ffa43f6e472d95e45746c295e8f72
Nico Carl <nicocarl@protonmail.com>
2022-11-26 12:55:07 +0100

    feat: Add resource for subscriptions (#244)
    Co-authored-by: Enzo Cappa <enzo.cappa@snyk.io>
    Co-authored-by: Cyril Gaudin <cyril.gaudin@gmail.com>

--- postgresql/provider.go
+++ postgresql/provider.go
@@ -171,6 +171,7 @@ func Provider() *schema.Provider {
 			"postgresql_grant_role":                resourcePostgreSQLGrantRole(),
 			"postgresql_replication_slot":          resourcePostgreSQLReplicationSlot(),
 			"postgresql_publication":               resourcePostgreSQLPublication(),
+			"postgresql_subscription":              resourcePostgreSQLSubscription(),
 			"postgresql_physical_replication_slot": resourcePostgreSQLPhysicalReplicationSlot(),
 			"postgresql_schema":                    resourcePostgreSQLSchema(),
 			"postgresql_role":                      resourcePostgreSQLRole(),
