e3b92a0fffc8ddf47761e4754cf9fe8012b20a0d
talbx <dev@talbx.de>
2023-08-27 16:29:19 +0200

    test: provided unit test for arePriviligesEqual function

--- postgresql/helpers_test.go
+++ postgresql/helpers_test.go
@@ -1,6 +1,7 @@
 package postgresql
 
 import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,4 +17,75 @@ func TestFindStringSubmatchMap(t *testing.T) {
 			"Body": "somehing_to_extract",
 		},
 	)
+}
+
+func TestArePrivilegesEqual(t *testing.T) {
+
+	type PrivilegesTestObject struct {
+		d         *schema.ResourceData
+		granted   *schema.Set
+		wanted    *schema.Set
+		assertion bool
+	}
+
+	tt := []PrivilegesTestObject{
+		{
+			buildResourceData("database", t),
+			buildPrivilegesSet("CONNECT", "CREATE", "TEMPORARY"),
+			all(),
+			true,
+		},
+		{
+			buildResourceData("database", t),
+			buildPrivilegesSet("CREATE", "USAGE"),
+			buildPrivilegesSet("USAGE"),
+			false,
+		},
+		{
+			buildResourceData("table", t),
+			buildPrivilegesSet("SELECT", "INSERT", "UPDATE", "DELETE", "TRUNCATE", "REFERENCES", "TRIGGER"),
+			all(),
+			true,
+		},
+		{
+			buildResourceData("table", t),
+			buildPrivilegesSet("SELECT"),
+			buildPrivilegesSet("SELECT, INSERT"),
+			false,
+		},
+		{
+			buildResourceData("schema", t),
+			buildPrivilegesSet("CREATE", "USAGE"),
+			all(),
+			true,
+		},
+		{
+			buildResourceData("schema", t),
+			buildPrivilegesSet("CREATE"),
+			all(),
+			false,
+		},
+	}
+
+	for _, configuration := range tt {
+		equal := arePrivilegesEqual(configuration.granted, configuration.wanted, configuration.d)
+		assert.Equal(t, configuration.assertion, equal)
+	}
+}
+
+func buildPrivilegesSet(grants ...interface{}) *schema.Set {
+	return schema.NewSet(schema.HashString, grants)
+}
+
+func all() *schema.Set {
+	return buildPrivilegesSet("ALL")
+}
+
+func buildResourceData(objectType string, t *testing.T) *schema.ResourceData {
+	var testSchema = map[string]*schema.Schema{
+		"object_type": {Type: schema.TypeString},
+	}
+	m := make(map[string]interface{})
+	m["object_type"] = objectType
+	return schema.TestResourceDataRaw(t, testSchema, m)
 }
