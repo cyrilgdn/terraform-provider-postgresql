c4f3658f0cabdd6cdfc7744d73cf258bd40435a3
“Yaron <yaron.mahat@riskified.com>
2024-02-01 12:21:40 +0200

    fix usage privs

--- postgresql/resource_postgresql_grant.go
+++ postgresql/resource_postgresql_grant.go
@@ -286,7 +286,12 @@ func readSchemaRolePriviges(txn *sql.Tx, db *DBConnection, d *schema.ResourceDat
 		dbName = "\"" + dbName + "\""
 	}
 	if !db.featureSupported(fetureAclExplode) {
-		query = `with a as ( show grants on schema $1 for $2) select array_agg(privilege_type) from a;`
+		query = `
+SELECT array_agg(privilege_type)
+FROM information_schema.role_table_grants
+WHERE table_schema = $1
+  AND grantee = $2;
+`
 	} else {
 		query = `
 SELECT array_agg(privilege_type)
