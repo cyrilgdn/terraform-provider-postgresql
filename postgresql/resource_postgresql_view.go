59042a7a979442658cd48818eec189a32fd56297
Nguyen <nvnguyen0089@gmail.com>
2024-05-02 23:09:19 +0700

    Read view definition from database

--- postgresql/resource_postgresql_view.go
+++ postgresql/resource_postgresql_view.go
@@ -1,18 +1,23 @@
 package postgresql
 
 import (
+	"bytes"
+	"database/sql"
 	"fmt"
+	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/lib/pq"
 )
 
 const (
 	viewNameAttr        = "name"
 	viewSchemaAttr      = "schema"
+	viewDatabaseAttr    = "database"
 	viewRecursiveAttr   = "recursive"
 	viewColumnNamesAttr = "column_names"
-	viewBodyAttr        = "body"
+	viewQueryAttr       = "query"
 	viewCheckOptionAttr = "check_option"
 )
 
@@ -28,12 +33,21 @@ func resourcePostgreSQLView() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
+			viewDatabaseAttr: {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: "The database where the view is located. If not specified, the provider default database is used.",
+
+				DiffSuppressFunc: defaultDiffSuppressFunc,
+			},
 			viewSchemaAttr: {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Computed:    true,
 				ForceNew:    true,
-				Description: "Schema where the view is located. If not specified, the provider default is used.",
+				Description: "The schema where the view is located. If not specified, the provider default is used.",
 
 				DiffSuppressFunc: defaultDiffSuppressFunc,
 			},
@@ -41,7 +55,7 @@ func resourcePostgreSQLView() *schema.Resource {
 				Type:        schema.TypeString,
 				Required:    true,
 				ForceNew:    true,
-				Description: "Name of the view.",
+				Description: "The name of the view.",
 			},
 			viewRecursiveAttr: {
 				Type:        schema.TypeBool,
@@ -50,15 +64,15 @@ func resourcePostgreSQLView() *schema.Resource {
 				Description: "If the view is a recursive view.",
 			},
 			viewColumnNamesAttr: {
-				Type:        schema.TypeList,
+				Type:        schema.TypeSet,
 				Optional:    true,
 				Elem:        &schema.Schema{Type: schema.TypeString},
 				Description: "The optional list of names to be used for columns of the view. If not given, the column names are deduced from the query.",
 			},
-			viewBodyAttr: {
+			viewQueryAttr: {
 				Type:        schema.TypeString,
 				Required:    true,
-				Description: "Body of the view.",
+				Description: "The query of the view.",
 
 				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
 					return normalizeFunctionBody(new) == old
@@ -72,7 +86,7 @@ func resourcePostgreSQLView() *schema.Resource {
 				Optional:         true,
 				DiffSuppressFunc: defaultDiffSuppressFunc,
 				ValidateFunc:     validation.StringInSlice([]string{"CASCADED", "LOCAL"}, true),
-				Description:      "Controls the behavior of automatically updatable views. One of: CASCADED, LOCAL",
+				Description:      "The check option which controls the behavior of automatically updatable views. One of: CASCADED, LOCAL",
 			},
 		},
 	}
@@ -94,9 +108,146 @@ func resourcePostgreSQLViewCreate(db *DBConnection, d *schema.ResourceData) erro
 }
 
 func resourcePostgreSQLViewReadImpl(db *DBConnection, d *schema.ResourceData) error {
+	viewID := d.Id()
+	if viewID == "" {
+		genViewID, err := genViewID(db, d)
+		if err != nil {
+			return err
+		}
+		viewID = genViewID
+	}
+
+	// Query the view definition
+	databaseName := db.client.databaseName
+	if databaseAttr, ok := d.GetOk(viewDatabaseAttr); ok {
+		databaseName = databaseAttr.(string)
+	}
+
+	query := ``
+	txn, err := startTransaction(db.client, databaseName)
+	if err != nil {
+		return err
+	}
+	defer deferredRollback(txn)
+
+	var viewQuery string
+	err = txn.QueryRow(query).Scan(&viewQuery)
+	switch {
+	case err == sql.ErrNoRows:
+		log.Printf("[WARN] PostgreSQL view: %s", viewID)
+		d.SetId("")
+		return nil
+	case err != nil:
+		return fmt.Errorf("Error reading view: %w", err)
+	}
+
+	if err := txn.Commit(); err != nil {
+		return err
+	}
+
+	d.Set(viewDatabaseAttr, databaseName)
+	d.Set(viewSchemaAttr, "")
+	d.Set(viewNameAttr, "")
+	d.Set(viewRecursiveAttr, false)
+	d.Set(viewColumnNamesAttr, "")
+	d.Set(viewQuery, "")
+	d.Set(viewCheckOptionAttr, "")
+
+	d.SetId(viewID)
+
 	return nil
 }
 
+func genViewID(db *DBConnection, d *schema.ResourceData) (string, error) {
+	// Generate with format: <database_name>.<schema_name>.<view_name>
+	b := bytes.NewBufferString("")
+	if databaseAttr, ok := d.GetOk(viewDatabaseAttr); ok {
+		fmt.Fprint(b, databaseAttr.(string), ".")
+	} else {
+		fmt.Fprint(b, db.client.databaseName, ".")
+	}
+
+	schemaName := "public"
+	if v, ok := d.GetOk(viewSchemaAttr); ok {
+		schemaName = v.(string)
+	}
+	viewName := d.Get(viewNameAttr).(string)
+
+	fmt.Fprint(b, schemaName, ".", viewName)
+	return b.String(), nil
+}
+
 func createView(db *DBConnection, d *schema.ResourceData, replace bool) error {
+	schemaName := "public"
+	if v, ok := d.GetOk(viewSchemaAttr); ok {
+		schemaName = v.(string)
+	}
+
+	name := d.Get(viewNameAttr).(string)
+	recursive := false
+	if v, ok := d.GetOk(viewRecursiveAttr); ok {
+		recursive = v.(bool)
+	}
+
+	var columnNames []string
+	if v, ok := d.GetOk(viewColumnNamesAttr); ok {
+		columnNames = v.([]string)
+	}
+
+	query := d.Get(viewQueryAttr).(string)
+
+	var checkOption string
+	if v, ok := d.GetOk(viewCheckOptionAttr); ok {
+		checkOption = v.(string)
+	}
+
+	// Construct the view
+	b := bytes.NewBufferString("CREATE ")
+	if replace {
+		b.WriteString("OR REPLACE ")
+	}
+
+	if recursive {
+		b.WriteString("RECURSIVE ")
+	}
+	b.WriteString("VIEW ")
+
+	fmt.Fprint(b, pq.QuoteIdentifier(schemaName), ".")
+	fmt.Fprint(b, pq.QuoteIdentifier(name))
+
+	for idx, columnName := range columnNames {
+		if idx <= 0 {
+			b.WriteRune('(')
+		}
+		if idx > 0 {
+			b.WriteRune(',')
+		}
+		b.WriteString(columnName)
+	}
+	if len(columnNames) > 0 {
+		b.WriteRune(')')
+	}
+
+	fmt.Fprint(b, " AS\n", query)
+	if checkOption == "" {
+		fmt.Fprint(b, "\nWITH ", checkOption, " CHECK OPTION")
+	}
+	b.WriteRune(';')
+
+	sql := b.String()
+	txn, err := startTransaction(db.client, d.Get(viewDatabaseAttr).(string))
+	if err != nil {
+		return err
+	}
+	defer deferredRollback(txn)
+
+	if _, err := txn.Exec(sql); err != nil {
+		return err
+	}
+
+	if err := txn.Commit(); err != nil {
+		return err
+	}
+
 	return nil
 }
